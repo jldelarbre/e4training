6745ad6850b789fac1691af6bc43703995743835
formation <formation@formation-Aspire-X3995>
2017-10-11 16:37:21 +0200

    Add color in tree view

--- com.sii.rental.ui/src/com/sii/rental/ui/views/RentalGuiElementProvider.java
+++ com.sii.rental.ui/src/com/sii/rental/ui/views/RentalGuiElementProvider.java
@@ -1,17 +1,42 @@
 package com.sii.rental.ui.views;
 
+
 import java.util.Collection;
 
+import org.eclipse.jface.viewers.IColorProvider;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
 
 import com.opcoach.training.rental.Customer;
 import com.opcoach.training.rental.Rental;
 import com.opcoach.training.rental.RentalAgency;
 import com.opcoach.training.rental.RentalObject;
 
-public class RentalGuiElementProvider extends LabelProvider implements ITreeContentProvider {
+public class RentalGuiElementProvider extends LabelProvider implements ITreeContentProvider, IColorProvider {
+
+	@Override
+	public Color getForeground(Object element) {
+		if (element instanceof Customer) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_DARK_MAGENTA);
+		}
+		if (element instanceof Rental) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_DARK_CYAN);
+		}
+		if (element instanceof RentalObject) {
+			return Display.getCurrent().getSystemColor(SWT.COLOR_DARK_GREEN);
+		}
+		return null;
+	}
+
+	@Override
+	public Color getBackground(Object element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
 	public Object[] getElements(Object inputElement) {
