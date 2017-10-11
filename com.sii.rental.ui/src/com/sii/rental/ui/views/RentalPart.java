b05df7969cafdb792d68020cedc4beb4e072b85b
formation <formation@formation-Aspire-X3995>
2017-10-11 10:19:47 +0200

    First HMI

--- com.sii.rental.ui/src/com/sii/rental/ui/views/RentalPart.java
+++ com.sii.rental.ui/src/com/sii/rental/ui/views/RentalPart.java
@@ -12,15 +12,20 @@ import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 
 import com.opcoach.training.rental.Rental;
+import com.opcoach.training.rental.RentalObject;
+import com.sii.rental.core.RentalCoreActivator;
 
 
 public class RentalPart {
 
 	private Label rentedObjectLabel;
+	private Label lblRentee;
+	private Label lblStartDate;
+	private Label lblEndDate;
 	
 	@PostConstruct
 	public void createContent(Composite parent) {
-		parent.setLayout(new GridLayout(1,false));
+		parent.setLayout(new GridLayout(1, false));
 		
 		Group infoGroup = new Group(parent, SWT.NONE);
 		infoGroup.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
@@ -32,6 +37,26 @@ public class RentalPart {
 		gd.horizontalSpan = 2;
 		gd.horizontalAlignment = SWT.FILL;
 		rentedObjectLabel.setLayoutData(gd);
+		
+		Label lblLoueA = new Label(infoGroup, SWT.NONE);
+		lblLoueA.setText("Loué à:");
+		
+		lblRentee = new Label(infoGroup, SWT.NONE);
+		
+		Group datesgroup = new Group(parent, SWT.NONE);
+		datesgroup.setLayout(new GridLayout(2, false));
+		
+		Label lblDu = new Label(datesgroup, SWT.NONE);
+		lblDu.setText("du:");
+		
+		lblStartDate = new Label(datesgroup, SWT.NONE);
+		
+		Label lblAu = new Label(datesgroup, SWT.NONE);
+		lblAu.setText("au:");
+		
+		lblEndDate = new Label(datesgroup, SWT.NONE);
+		
+		setRental(RentalCoreActivator.getAgency().getRentals().get(0));
 	}
 
 	@Focus
@@ -41,7 +66,10 @@ public class RentalPart {
 	
 	public void setRental(Rental r)
 	{
-		rentedObjectLabel.setText(r.getRentedObject().getName());
+		RentalObject rentedObject = r.getRentedObject();
+		rentedObjectLabel.setText(rentedObject.getName());
+		lblRentee.setText(r.getCustomer().getFirstName() + " " + r.getCustomer().getLastName());
+		lblStartDate.setText(r.getStartDate().toString());
+		lblEndDate.setText(r.getEndDate().toString());
 	}
-
 }
